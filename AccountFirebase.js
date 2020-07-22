ccfb8264dce84f7cb155f944253fcc40808772d0
JxKnight <61744719+JxKnight@users.noreply.github.com>
2020-07-22 20:25:16 +0800

    Initial Commit

---
+++ AccountFirebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB7dODmWMrTxwqx7kuOd1tPH6A6v6D3hPM",
+    authDomain: "unilife-33d2e.firebaseapp.com",
+    databaseURL: "https://unilife-33d2e.firebaseio.com",
+    projectId: "unilife-33d2e",
+    storageBucket: "unilife-33d2e.appspot.com",
+    messagingSenderId: "230547566060",
+    appId: "1:230547566060:web:de66da2c1d9e000e76a4c9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
